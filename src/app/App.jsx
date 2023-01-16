6269b108560e4878ceecc5d9e73bd581d3149578
“nicm42” <42087659+nicm42@users.noreply.github.com>
2023-01-16 20:05:42 +0000

    Added loading message and try catch to API fetch

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -3,19 +3,26 @@ import Name from './components/Name';
 import './App.css';
 
 function App() {
+  const [loading, setLoading] = useState(false);
   const [boys, setBoys] = useState([]);
   const [girls, setGirls] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
-      const response = await fetch('/api');
-      const data = await response.json();
-      console.log(data.docs[0]);
-      setBoys(data.docs.filter((hobbit) => hobbit.gender === 'Male'));
-      setGirls(data.docs.filter((hobbit) => hobbit.gender === 'Female'));
+      try {
+        setLoading(true);
+        const response = await fetch('/api');
+        const data = await response.json();
+        console.log(data.docs[0]);
+        setLoading(false);
+        setBoys(data.docs.filter((hobbit) => hobbit.gender === 'Male'));
+        setGirls(data.docs.filter((hobbit) => hobbit.gender === 'Female'));
+      } catch (error) {
+        setLoading(false);
+        console.error(error);
+      }
     };
 
-    //fetchData();
     setBoys([
       { _id: '11', name: 'Bilbo Baggins' },
       { _id: '12', name: 'Gorhendad (Oldbuck) Brandybuck' },
@@ -24,10 +31,12 @@ function App() {
       { _id: '21', name: 'Pearl Took' },
       { _id: '22', name: 'Peony (Baggins) Burrows' },
     ]);
+    //fetchData();
   }, []);
 
   return (
     <div className="App">
+      {loading && <div>Loading...</div>}
       <div>Boys:</div>
       <ul>
         {boys &&
