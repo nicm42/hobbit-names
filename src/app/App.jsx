2ded57b22862d22ecaf6a4b0aed352f7f30008b6
“nicm42” <42087659+nicm42@users.noreply.github.com>
2023-01-16 19:48:44 +0000

    Got hobbit names in boys and girls lists

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -2,19 +2,36 @@ import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  //const [count, setCount] = useState(0)
+  const [boys, setBoys] = useState([]);
+  const [girls, setGirls] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
       const response = await fetch('/api');
       const data = await response.json();
-      console.log(data.docs);
+      console.log(data.docs[0]);
+      setBoys(data.docs.filter((hobbit) => hobbit.gender === 'Male'));
+      setGirls(data.docs.filter((hobbit) => hobbit.gender === 'Female'));
     };
 
-    fetchData();
+    //fetchData();
+    setBoys([
+      { _id: '11', name: 'Bilbo Baggins' },
+      { _id: '12', name: 'Gorhendad (Oldbuck) Brandybuck' },
+    ]);
+    setGirls([
+      { _id: '21', name: 'Pearl Took' },
+      { _id: '22', name: 'Peony (Baggins) Burrows' },
+    ]);
   }, []);
 
-  return <div className="App">testing</div>;
+  return (
+    <div className="App">
+      <div>Boys:</div>
+      {boys.map((hobbit) => hobbit.name.split(' ')[0])}
+      {girls.map((hobbit) => hobbit.name.split(' ')[0])}
+    </div>
+  );
 }
 
 export default App;
