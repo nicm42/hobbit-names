801c462f93b87747a80d9e5bd36a04eb7917c980
“nicm42” <42087659+nicm42@users.noreply.github.com>
2023-02-05 12:30:04 +0000

    Added loading text for screen readers

--- src/app/components/Loading.jsx
+++ src/app/components/Loading.jsx
@@ -3,6 +3,7 @@ import './Loading.css';
 function Loading() {
   return (
     <div className="loading">
+      <p className="sr-only">Loading</p>
       <div className="dot" />
       <div className="dot" />
       <div className="dot" />
--- src/app/components/Loading.test.jsx
+++ src/app/components/Loading.test.jsx
@@ -3,16 +3,10 @@ import Loading from './Loading';
 
 describe('Loading component tests', () => {
   beforeEach(() => {
-    render(<Loading />);
+    const loading = render(<Loading />);
   });
 
-  test('it renders without crashing', () => {
-    //render(<Loading />);
-    //const loading = render(<Loading />);
-    //expect(loading).toBeTruthy();
-    //const loadingDiv = screen.getByTestId('loading');
-    //expect(loadingDiv).toBeInTheDocument();
-    //screen.debug();
-    expect(true).toBeTruthy();
+  test('should show loading text', () => {
+    expect(screen.getByText(/Loading/i)).toBeInTheDocument();
   });
 });
