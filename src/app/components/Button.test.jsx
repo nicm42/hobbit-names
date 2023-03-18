d0916bb69af7a437fcb5d32eb1fa21621fc95cfb
“nicm42” <42087659+nicm42@users.noreply.github.com>
2023-03-18 17:20:45 +0000

    Changed fireEvent to useEvent

--- src/app/components/Button.test.jsx
+++ src/app/components/Button.test.jsx
@@ -1,4 +1,5 @@
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Button from './Button';
 
 describe('Button component tests', () => {
@@ -15,12 +16,12 @@ describe('Button component tests', () => {
   });
 
   test('should show with relevant text and value', () => {
+    expect(screen.getByRole('button')).toBeInTheDocument();
     expect(screen.getByText(/Show boys names/i)).toBeInTheDocument();
   });
 
-  test('should call setGenderToShow on clicking', () => {
-    const button = screen.getByText(/Show boys names/i);
-    fireEvent.click(button);
+  test('should call setGenderToShow on clicking', async () => {
+    await userEvent.click(await screen.findByRole('button', { value: 'boys' }));
     expect(setGenderToShow).toHaveBeenCalledWith('boys');
   });
 });
